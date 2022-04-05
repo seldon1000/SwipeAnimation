4dcd6d81f80bdea71fa03a4f0f2bff66c686b581
Nicolas Mariniello <seldon1000@tutanota.com>
2022-04-05 10:36:56 +0200

    Initial Commit

---
+++ SwipeAnimation/SwipeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAnimationApp.swift
+//  SwipeAnimation
+//
+//  Created by Nicolas Mariniello on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
